15f6bd4d61eb33382f43c236acd1ab17f23addd6
ParsaMashayekhy <140300504+Parsa-Mashayekhy-Fard@users.noreply.github.com>
2023-07-30 13:52:46 +0330

    Add files via upload

--- Assignment2.2.py
+++ Assignment2.2.py
@@ -1,8 +1,11 @@
-a = float(input("enter your first side: "))
-b = float(input("enter your second  side:"))
-c = float(input("enter your third side: "))
+a = float(input("Enter your first side: "))
+b = float(input("Enter your second side: "))
+c = float(input("Enter your third side: "))
 
-if a+b>c and a+c>b and b+c>a and a!=0 and b!=0 and c!=0 :
-    print("These number can form a triangle")
+if a > 0 and b > 0 and c > 0:
+    if a + b > c and a + c > b and b + c > a:
+        print("These numbers can form a triangle.")
+    else:
+        print("These numbers can't form a triangle.")
 else:
-    print("These number can't form a triangle") 
+    print("Invalid side lengths. All sides must be greater than zero to form a triangle.")
